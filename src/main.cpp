44f61a3017cc6efa451ca401c757c7ee7f1595d7
brandon.grando <brandon.grando@team.aussiebroadband.com.au>
2025-11-25 16:48:26 +1100

    built out rest of MVP context manager

--- src/main.cpp
+++ src/main.cpp
@@ -3,58 +3,40 @@
 #include <boost/multiprecision/cpp_int.hpp>
 
 #include "dhke/key_gen.hpp"
-#include "dhke/particiant.hpp"
+#include "dhke/participant.hpp"
+#include "dhke/context_handler.hpp"
 
 int main()
 {
     spdlog::info("Starting DH key demo");
 
-    // a small integer (i.e. 2 or 5) is appropriate as the generator
-    int publicGenerator = 5;
-    // generate required public prime, and private keys
-    boost::multiprecision::cpp_int prime512 = KeyGenerator::getPrimeNumber(2048);
-    boost::multiprecision::cpp_int bobPrivateKey = KeyGenerator::getLargeRandomInt(2, 2000);
-    boost::multiprecision::cpp_int alicePrivateKey = KeyGenerator::getLargeRandomInt(2, 2000);
+    // set up key exchange context
+    DHKEParticipant Bob = DHKEParticipant("Bob");
+    DHKEParticipant Alice = DHKEParticipant("Alice");
+    DHKEContextManager manager = DHKEContextManager(Bob, Alice);
 
-    // create Alice and Bob
-    DHKEParticipant Bob = DHKEParticipant(publicGenerator, prime512, bobPrivateKey, "Bob");
-    DHKEParticipant Alice = DHKEParticipant(publicGenerator, prime512, alicePrivateKey, "Alice");
-
-    // run step 1 -> public keys generation
-    boost::multiprecision::cpp_int bobStep1 = Bob.step1();
-    boost::multiprecision::cpp_int aliceStep1 = Alice.step1();
-
-    // run step 2 -> swap public keys and compute final shared secret key
-    boost::multiprecision::cpp_int bobStep2 = Bob.step2(aliceStep1);
-    boost::multiprecision::cpp_int aliceStep2 = Alice.step2(bobStep1);
-
-    std::cout << "Bob shared secret:   " << bobStep2 << std::endl;
-    std::cout << "Alice shared secret: " << aliceStep2 << std::endl;
-
-    std::cout << "Are the two shared secrets the same? - ";
-    if (bobStep2 == aliceStep2)
-    {
-        std::cout << "TRUE" << std::endl;
-    }
-    else
-    {
-        std::cout << "FALSE" << std::endl;
-    }
+    // generate params
+    manager.generateParameters(512);
+    // run step 1
+    manager.step1();
+    // run step 2
+    manager.step2();
+    // validate
+    manager.validateSharedSecret();
 
     return 0;
 }
 
-
 // testing large prime number generation
-    // boost::multiprecision::cpp_int prime64 = KeyGenerator::getPrimeNumber(/*64*/);
-    // boost::multiprecision::cpp_int prime128 = KeyGenerator::getPrimeNumber(128);
-    // boost::multiprecision::cpp_int prime256 = KeyGenerator::getPrimeNumber(256);
-    // boost::multiprecision::cpp_int prime512 = KeyGenerator::getPrimeNumber(512);
-    // boost::multiprecision::cpp_int prime1024 = KeyGenerator::getPrimeNumber(1024);
-    // boost::multiprecision::cpp_int prime2048 = KeyGenerator::getPrimeNumber(2048);
+// boost::multiprecision::cpp_int prime64 = KeyGenerator::getPrimeNumber(/*64*/);
+// boost::multiprecision::cpp_int prime128 = KeyGenerator::getPrimeNumber(128);
+// boost::multiprecision::cpp_int prime256 = KeyGenerator::getPrimeNumber(256);
+// boost::multiprecision::cpp_int prime512 = KeyGenerator::getPrimeNumber(512);
+// boost::multiprecision::cpp_int prime1024 = KeyGenerator::getPrimeNumber(1024);
+// boost::multiprecision::cpp_int prime2048 = KeyGenerator::getPrimeNumber(2048);
 
-    // testing large random int generation
-    // KeyGenerator::getLargeRandomInt(2, 10);
-    // KeyGenerator::getLargeRandomInt(2, 100);
-    // KeyGenerator::getLargeRandomInt(2, 1000);
-    // KeyGenerator::getLargeRandomInt(2, 10000);
+// testing large random int generation
+// KeyGenerator::getLargeRandomInt(2, 10);
+// KeyGenerator::getLargeRandomInt(2, 100);
+// KeyGenerator::getLargeRandomInt(2, 1000);
+// KeyGenerator::getLargeRandomInt(2, 10000);
