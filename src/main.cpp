de2392ac50f3278be43b3f506bb1a11081bf1363
bgrando24 <bgrando24@gmail.com>
2025-12-05 22:31:14 +1100

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,3 @@ int main()
 
     return 0;
 }
-
-// testing large prime number generation
-// boost::multiprecision::cpp_int prime64 = KeyGenerator::getPrimeNumber(/*64*/);
-// boost::multiprecision::cpp_int prime128 = KeyGenerator::getPrimeNumber(128);
-// boost::multiprecision::cpp_int prime256 = KeyGenerator::getPrimeNumber(256);
-// boost::multiprecision::cpp_int prime512 = KeyGenerator::getPrimeNumber(512);
-// boost::multiprecision::cpp_int prime1024 = KeyGenerator::getPrimeNumber(1024);
-// boost::multiprecision::cpp_int prime2048 = KeyGenerator::getPrimeNumber(2048);
-
-// testing large random int generation
-// KeyGenerator::getLargeRandomInt(2, 10);
-// KeyGenerator::getLargeRandomInt(2, 100);
-// KeyGenerator::getLargeRandomInt(2, 1000);
-// KeyGenerator::getLargeRandomInt(2, 10000);
